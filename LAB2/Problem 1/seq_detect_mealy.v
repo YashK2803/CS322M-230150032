905661a6da4906af1e80e29dc30db7f032521981
YashK2803 <kharangateyash@gmail.com>
2025-08-17 22:30:50 +0530

    LAB 2: Problems 1,2

---
+++ LAB2/Problem 1/seq_detect_mealy.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module seq_detect_mealy(input clk,din,reset,output y);
+parameter A=2'd0,B=2'd1,C=2'd2,D=2'd3;
+reg [1:0] state,next_state;
+always@(*) begin
+case(state)
+A:next_state = din?B:A;
+B:next_state = din?C:A;
+C:next_state = din?C:D;
+D:next_state = din?B:A;
+endcase
+end
+always@(posedge clk) begin
+if(reset) state<=A;
+else state<=next_state;
+end
+assign y = (state==D)&din;
+endmodule
